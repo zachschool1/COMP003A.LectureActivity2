bd8ceeff7e74180f8ee4134aee5152e117e38c8a
zachschool1 <zacharyjordan@my.whccd.edu>
2026-01-21 19:08:20 -0800

    Add project files.

---
+++ COMP003A.LectureActivity2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace COMP003A.LectureActivity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number.");
+            string first = Console.ReadLine();
+
+            
+            
+            Console.WriteLine("Enter the second number.");
+            string second = Console.ReadLine();
+
+
+            var dfirst = double.Parse(first);
+            var dsecond = double.Parse(second);
+
+            var sum = dfirst + dsecond;
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine($"Sum41: {sum}.");
+
+            var average = (dfirst + dsecond) / 2;
+            Console.WriteLine("Avg: " + average);
+            Console.WriteLine($"Average: {average}");
+
+            bool greater = dfirst > dsecond;
+            bool notEqual = dfirst != dsecond;
+            Console.WriteLine(greater);
+            Console.WriteLine(notEqual);
+        }
+    }
+}
